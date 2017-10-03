adc18207de5d51d5f205d6b1bdc7fa82917cc938
Ryan Kang <pinkmaguro137@gmail.com>
2017-10-03 11:04:55 +0900

    first template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
-
+from .models import Question, Choice
 
 def index(request):
     current_time = datetime.datetime.now()
-    context = {current_time : current_time}
     
-    return HttpResponse('Hello Django.')
-    # return render(request, 'polls/index.html')
+    #return HttpResponse('Hello Django.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list,
+                'name':'Ryan Kang',
+                'today': current_time}  # strftime('%Y-%m-%d')
+    return render(request, 'polls/index.html', context)
+
