5d01ccfb128f3ce8f01be1eea15fa74c0bb868b3
Ryan Kang <pinkmaguro137@gmail.com>
2017-10-03 09:17:19 +0900

    02 first app, firstview, HttpResponse

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+
+def index(request):
+    current_time = datetime.datetime.now()
+    context = {current_time : current_time}
+    
+    return HttpResponse('Hello Django.')
+    # return render(request, 'polls/index.html')
