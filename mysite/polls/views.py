091722fe0c2d34efcce1d904b930d9b2ece19819
Ryan Kang <pinkmaguro137@gmail.com>
2017-10-03 15:06:21 +0900

    test model logic

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 import datetime
 from .models import Question, Choice
@@ -52,12 +53,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+                    pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    s
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
